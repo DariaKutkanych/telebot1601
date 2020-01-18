194dfbfb03ba5d78ac71eac2c7ba8e81982efa60
DariaKutkanych <d.goubenko@gmail.com>
2020-01-18 12:26:27 +0200

    flask sync

--- main.py
+++ main.py
@@ -2,6 +2,24 @@ from telebot import TeleBot
 import requests
 from telebot import types
 import re
+from flask import Flask, request
+import git
+
+app = Flask(__name__)
+
+
+@app.route('/update_server', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('https://github.com/DariaKutkanych/telebot1601.git')
+        origin = repo.remotes.origin
+
+        origin.pull()
+
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
+
 
 bot = TeleBot("1007314497:AAHvjuAFIIN0GpUOevA3HturouG_tyS2hQ0")
 
